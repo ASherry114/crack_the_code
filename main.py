631e605db3002eee2edc62f2602672197a186be6
ASherry114 <andrew.sherry@proton.me>
2024-03-17 09:36:11 +0800

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Crack the code board game.
+Hosts it on a webserver and then you can play it on your browser.
+Mastermind-esque style game.
+"""
+
+
+def main():
+    # Get server running
+
+    # Get game running
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/game.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Crack the code board game.
+"""
+
+
+class Game:
+    """
+    Crack the code game class.
+    Tracks the state of the game and handles the game logic.
+
+    Each player collects numbers which are then arranged in a specific order.
+    The players must ask and answer questions to figure out what numbers the
+    other players have taken, and then deduce the remaining numbers to win the
+    game.
+    """
+
